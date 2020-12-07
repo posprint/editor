19370d471cd8891a6f0b481dd918469677f2f00c
Halbert Lei <princescar@gmail.com>
2020-12-07 17:49:29 +0800

    add error tip.

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useEffect, useRef, useState } from 'react';
-import { Alignment, Button, ButtonGroup, Classes, Dialog, FormGroup, InputGroup, Intent, MenuItem, Navbar, Tab, Tabs, Tag, Toaster } from '@blueprintjs/core';
+import { Alignment, Button, ButtonGroup, Classes, Dialog, FormGroup, Icon, InputGroup, Intent, MenuItem, Navbar, Tab, Tabs, Tag, Toaster, Tooltip } from '@blueprintjs/core';
 import { Select } from "@blueprintjs/select";
 import Split from 'react-split';
 import ipRegex from 'ip-regex';
@@ -15,6 +15,18 @@ const allEditorPanels = [
   { key: 'data', title: 'Data' },
 ];
 
+class HyrdateError extends Error {
+  private _error: Error
+  private _type: 'TEMPLATE' | 'STYLE' | 'DATA'
+  get type() { return this._type }
+
+  constructor(type: 'TEMPLATE' | 'STYLE' | 'DATA', error: Error) {
+    super(error.message)
+    this._error = error
+    this._type = type
+  }
+}
+
 const App: FC = () => {
   const toaster = useRef<Toaster>(null);
 
@@ -27,6 +39,7 @@ const App: FC = () => {
   const [encoding, setEncoding] = usePersistState<string>('encoding', 'GBK');
 
   const [dom, setDom] = useState();
+  const [hydrateError, setHydrateError] = useState<HyrdateError>();
   const [showSettings, setShowSettings] = useState<boolean>(false);
   const [printerAddressInvalid, setPrinterAddressInvalid] = useState<boolean>(false);
   const [printer, setPrinter] = useState<IPrinter>();
@@ -52,29 +65,31 @@ const App: FC = () => {
   // Try to hydrate when code changed.
   useEffect(() => {
     const timer = setTimeout(() => {
-      if (!template) return;
+      try {
+        if (!template) return;
 
-      let style: any, data: any;
-      try {
-        style = JSON.parse(styleString || '{}');
-      } catch (e) {
-        console.error(`Style error: ${e.message}`);
-        return
-      }
-      try {
-        data = JSON.parse(dataString || '{}');
-      } catch (e) {
-        console.error(`Data error: ${e.message}`);
-        return
-      }
+        let style: any, data: any;
+        try {
+          style = JSON.parse(styleString || '{}');
+        } catch (e) {
+          throw new HyrdateError('STYLE', e)
+        }
+        try {
+          data = JSON.parse(dataString || '{}');
+        } catch (e) {
+          throw new HyrdateError('DATA', e)
+        }
 
-      try {
-        const dom = hydrate(template, style, data, {});
-        setDom(dom);
-        console.debug(JSON.stringify(dom));
+        try {
+          const dom = hydrate(template, style, data, {});
+          setDom(dom);
+          console.debug(JSON.stringify(dom));
+        } catch (e) {
+          throw new HyrdateError('TEMPLATE', e)
+        }
+        setHydrateError(undefined)
       } catch (e) {
-        console.error(`Hydrate error: ${e.message}`);
-        return
+        setHydrateError(e)
       }
     }, 500);
     return () => clearTimeout(timer);
@@ -189,16 +204,31 @@ const App: FC = () => {
             />
           )}
         </Split>
-        <div className="sidebar">
+        <div className={'sidebar' + (hydrateError ? ' has-error' : '')}>
           {dom && (
             <>
-              <Tabs className='paper-size-selector' selectedTabId={escPaperWidth} onChange={x => setEscPaperWidth(x as number)}>
+              <Tabs className="paper-size-selector" selectedTabId={escPaperWidth} onChange={x => setEscPaperWidth(x as number)}>
                 <Tab id={80} title="80 mm" />
                 <Tab id={72} title="72 mm" />
                 <Tab id={58} title="58 mm" />
               </Tabs>
-              <div className='preview' style={{ width: `${escPaperWidth * 3.5}px` }}>
+              <div className="preview" style={{ width: `${escPaperWidth * 3.5}px` }}>
                 <Previewer dom={dom} />
+                {hydrateError && (
+                  <Tooltip
+                    className="error-tooltip"
+                    position="bottom-left"
+                    intent="danger"
+                    content={(
+                      <div>
+                        <strong>{hydrateError.type} ERROR</strong>
+                        <pre>{hydrateError.stack}</pre>
+                      </div>
+                    )}
+                  >
+                    <Icon intent="danger" icon="warning-sign" />
+                  </Tooltip>
+                )}
               </div>
             </>
           )}
